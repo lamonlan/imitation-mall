ba87d18fec4d043ab9769c3c16b42ee7a5be8e70
lamonlan <2470413700@qq.com>
2020-09-26 19:23:07 +0800

    second commit

---
+++ src/components/common/TabBarItem.vue
@@ -0,0 +1,60 @@
+<template>
+      <div class="tab-bar-item" @click="itemClick">
+          <!-- <img src="../../assets/img/tabbar/首页.svg"/>
+          <div>首页</div> -->
+          <div v-if="!isActive"><slot name="item-icon"></slot></div>
+          <div v-else><slot name="item-icon-active"></slot></div>
+          <!-- <div :class="{active:isActive}"><slot name="item-text"></slot></div> -->
+          <div :style="activeStyle"><slot name="item-text"></slot></div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props: {
+      path: String,
+      activeColor: {
+          type: String,
+          default: 'pink'
+      }
+  },
+  data () {
+      return {
+        //   isActive:true
+      }
+  },
+  computed: {
+      isActive(){
+          // indexOf判断当前路径是否和我点击的路径相同，不相同的话=-1，所以判断不等于-1即为相同
+          // 用$route是因为此处取的是路由的path
+          return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle () {
+          return this.isActive?{color:this.activeColor}:{}
+      }
+  },
+  methods: {
+      itemClick(){
+          this.$router.push(this.path)
+      }
+  }
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
@@ -17,6 +21,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import('../views/cart/Cart.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../views/profile/Profile.vue')
+  },
+  {
+    path: '/category',
+    name: 'Category',
+    component: () => import('../views/category/Category.vue')
   }
 ]
 
