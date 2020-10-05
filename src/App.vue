f767f9e7f62355b36dccff6acd699d6080d4b260
lamonlan <2470413700@qq.com>
2020-10-05 11:54:43 +0800

    首页推荐与HomeControl

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import MainTabBar from './components/connect/MainTabBar'
+import MainTabBar from './components/connect/MainTabBar/MainTabBar'
 export default {
   name: 'App',
   components: {
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,50 @@
       <nav-bar class="home-nav"><div slot="center">imitation-mall</div></nav-bar>
       <home-swiper :banners="banners"></home-swiper>
       <home-recommends :recommends="recommends"></home-recommends>
+      <home-feature></home-feature>
+      <main-control :titles="['流行','新款','精选']" class="tab-control"></main-control>
+      <ul>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+        <li>li</li>
+      </ul>
 </div>
 </template>
 
@@ -10,6 +54,10 @@
 import NavBar from '../../components/common/navbar/NavBar'
 import HomeSwiper from './childComponents/HomeSwiper'
 import HomeRecommends from './childComponents/HomeRecommends'
+import HomeFeature from './childComponents/HomeFeature'
+
+import MainControl from 'components/connect/mainControl/MainControl'
+
 import {getHomeMultidata} from '../../network/home'
 
 
@@ -18,7 +66,9 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    HomeRecommends
+    HomeRecommends,
+    MainControl,
+    HomeFeature
   },
   data() {
     return {
@@ -40,9 +90,23 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: aliceblue;
   text-align: center;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+
+.tab-control{
+  position: sticky;
+  top: 44px;
 }
 </style>>
