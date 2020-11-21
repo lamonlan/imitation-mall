4717dda114bd4bdd8f2386e4f2e0b2f82b684576
lamonlan <2470413700@qq.com>
2020-11-21 22:17:10 +0800

    详情页面的轮播图和滚动效果以及内容的显示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -13,28 +13,56 @@
                             {{item}}</center-bar>
             </div>
         </nav-bar>
+        <scroll class="content">
+        <detail-swiper :top-images="topImages"></detail-swiper>
+        <detail-base-info :goods="goods"></detail-base-info>
+        <detail-shop-info :shop="shop"></detail-shop-info>
+        </scroll>
     </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import CenterBar from './centerbar/CenterBar'
+import DetailSwiper from './centerbar/DetailSwiper'
+import DetailBaseInfo from './centerbar/DetailBaseInfo'
+import DetailShopInfo from './centerbar/DetailShopInfo'
+import Scroll from 'components/common/scroll/Scroll'
+
+import {getTopImages,Goods,Shop} from './detailnetwork/detail'
 
 export default {
     name: 'Detail',
     components: {
         NavBar,
-        CenterBar
+        CenterBar,
+        DetailSwiper,
+        DetailBaseInfo,
+        DetailShopInfo,
+        Scroll
     },
     data(){
         return{
             iid: null,
             titles: ['商品','参数','评论','推荐'],
-            currentIndex: 0
+            currentIndex: 0,
+            topImages: [],
+            goods: {},
+            shop: {}
         }
     },
     created(){
         this.iid = this.$route.params.iid
+     
+        getTopImages(this.iid).then(res => {
+            console.log(res)
+            const data = res.data.result
+            this.topImages = data.itemInfo.topImages
+            this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+            this.shop = new Shop(data.shopInfo)
+        })
+
+        
     },
     methods: {
         titleClick(index){
@@ -48,8 +76,27 @@ export default {
 </script>
 
 <style scoped>
+.detail-page{
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    /* height: 100vh; */
+}
+
+.content{
+    /* 此方法使得topnav无法一直显示在页面上 */
+    /* height: calc(100% - 44px); */
+    position: fixed;
+    top: 44px;
+    left: 0;
+    overflow: hidden;
+    bottom: 46px;
+}
+
 .detail-nav{
-    text-align: center;
+    display: relative;
+    z-index: 9;
+    background-color: #fff;
 }
 .left{
     padding-top: 5px;
---
+++ src/views/detail/centerbar/DetailSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+    <swiper class="detailswiper">
+        <swiper-item v-for="item in topImages" :key="item">
+            <img :src="item">
+        </swiper-item>
+    </swiper>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from 'components/common/swiper/index'
+
+export default {
+    name: 'DetailSwiper',
+    components: {
+        Swiper,
+        SwiperItem
+    },
+    props: {
+        topImages: {
+            type: Array,
+            default(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.detailswiper{
+    height: 300px;
+}
+</style>
